66f18d8400d534f0edf7aeea272f466dc24dfa5e
Saad Alharbi <sharbi@tetco.sa>
2024-10-14 14:59:25 +0300

    tailwaind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-
-
 function App() {
-
   return (
-    <>
-      
-    </>
+    <main className="py-10 h-screen">
+      <h1 className="font-bold text-3xl text-center">hello</h1>
+    </main>
   )
 }
 
