0bea3ea7b656d7e7226b39a8e52f97f9f94e66f4
Saad Alharbi <sharbi@tetco.sa>
2024-10-17 12:30:31 +0300

    shadcn added

--- src/App.tsx
+++ src/App.tsx
@@ -13,24 +13,27 @@ function App() {
     deletTodo,
     deleteAllCompletedTodos,
   } = useTodo();
+
   return (
-    <main className="py-10 h-screen overflow-y-auto">
-      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
-      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
-        <AddTodoForm
-          onSubmit={addTodo}
-        />
-        <TodoList
+    <>
+      <main className="py-10 h-screen overflow-y-auto">
+        <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+        <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+          <AddTodoForm
+            onSubmit={addTodo}
+          />
+          <TodoList
+            todos={todos}
+            onCompletedChange={setTodoCompleted}
+            onDelete={deletTodo}
+          />
+        </div>
+        <TodoSummary
           todos={todos}
-          onCompletedChange={setTodoCompleted}
-          onDelete={deletTodo}
+          deleteAllCompleted={deleteAllCompletedTodos}
         />
-      </div>
-      <TodoSummary
-        todos={todos}
-        deleteAllCompleted={deleteAllCompletedTodos}
-      />
-    </main>
+      </main>
+    </>
   )
 }
 
